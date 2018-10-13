14f67d89c0d571a668be5fdb1c75c69bd926eabe
Lucy Suddenly <suddenlylucy@gmail.com>
2018-10-13 18:45:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-
+function getFirstSelector(selector) {
+ return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector(`#nested .target`)
+}
+function increaseRankBy(n) {
+  const rankUp = document
+  .querySelectorAll('.ranked-list li')
+  for (var i = 0; rankUp.length > i; i++) {
+   rankUp[i].innerHTML = (parseInt(rankUp[i].innerHTML) + n).toString()
+  }
+}
+function deepestChild() {
+ const element = document
+ .getElementById('grand-node')
+ const childOfElement = element.children[0]
+ while (childOfElement){
+   if (querySelector('div') === null) {
+     
+   }
+ }
+}
